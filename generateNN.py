f7d659cc5949b15d071f66498b01c4a31eb462d9
Bert Yu <ryu2020@pingry.org>
2018-11-09 15:20:08 -0500

    Update generateNN.py

--- generateNN.py
+++ generateNN.py
@@ -6,14 +6,17 @@ make neural net:
 in: number of nodes in input layer
 vec: numbers of nodes in hidden layers
 out: number of nodes in output layer
-shape: shape of input data
+shape: shape of input data (tuple describing range, with None to show no limit in range)
 act: activation function ('sigmoid' or 'tanh' most likely)
+TODO?: add initialization arguments 
 '''
 
-def make(in, vec, out, shape, act):
-    net = new Sequential()
-    net.add(Dense(in, input_shape = shape, activation = act))
+def make(ins, vec, out, shape, act):
+    net = Sequential()
+    net.add(Dense(ins, input_shape = shape, activation = act))
     for i in vec:
         net.add(Dense(i, activation = act))
-    net.add(Dense(out, act = act))
+    net.add(Dense(out, activation = act))
     return net
+
+make(1, [1], 1, (0,1), 'tanh')
