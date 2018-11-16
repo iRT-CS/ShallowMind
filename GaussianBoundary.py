14f0019ea0ef1cfb668866d8d8855ed6bcbd31fd
Aditya Gollapudi <agollapudi2019@pingry.org>
2018-11-16 10:00:01 -0500

    Fixed numpy.polyval
    Gaussian function has denominator removed so peak is controlled differently.

--- GaussianBoundary.py
+++ GaussianBoundary.py
@@ -1,6 +1,6 @@
-from random import randrange, gauss
 import numpy as np
 from scipy.optimize import fmin_cobyla
+import math
 #import timeit
 def genFunctionUniform(degree = 2,minimum = -7,maximum = 7):
     coefficients = []
@@ -22,8 +22,10 @@ def genFunctionGaussian(degree = 2,mean = 0,sigma = 7, scaling = False):
 
 def evalFunction(coVec,ipVar):
     boundaryPoint = 0
+    maximum = len(coVec) - 1
     for i in range(len(coVec)):
-        termVal = pow(ipVar,i)*coVec[i]
+        power = maximum - i
+        termVal = pow(ipVar,power)*coVec[power]
         boundaryPoint += termVal
     return boundaryPoint
 def pointDistance(x1,y1,x2,y2):
@@ -37,14 +39,28 @@ def distanceToCurve(coVec,ipVar,dpVar):
         cons = [lambda x: abs(ipVar-maxDistance)],\
         rhoend = 1e-3)
     return pointDistance(ipVar,dpVar,xMin[0], evalFunction(coVec, xMin[0]))
-
-def getGuassianAtPoint()
-def categorize(ipVar,dpVar,coVec,noiseRate):
-    boundaryPoint = evalFunction(coVec,ipVar)
-    rawCategory = (dpVar >= boundaryPoint)
-    #Find noise
-    distanceToCurve = distanceToCurve(coVec,ipVar,dpVar)
-    
+def gauss(distance, sigma):
+    sigmaComponent = pow(sigma,2)*2
+    #denominator = math.sqrt(math.pi*sigmaComponent)
+    #I want to control the max height
+    denominator = 1
+    numerator = math.exp(-pow(distance,2)/sigmaComponent)
+    return numerator/denominator
+def getPoints(coVec,numPoints,sigma,peak,xMin,xMax,yMin,yMax):
+    x = np.random.rand(numPoints)
+    xRange = xMax - xMin
+    x = list(map(lambda v: (v*xRange)+xMin,x))
+    y = np.random.rand(numPoints)
+    yRange = yMax - yMin
+    y = list(map(lambda v: (v*yRange)+yMin,y))
+    boundaryPoints = np.polyval(coVec,x)
+    distances = peak*list(map(lambda m,n: distanceToCurve(coVec,m,n),x,y))
+    gaussian = list(map(lambda d: gauss(d,sigma),distances))
+    flip = list(map(lambda g: (np.random.uniform()<g),gaussian))
+    cleanVals = list(map(lambda d,b: (d>b),y,boundaryPoints))
+    dirtyVals = list(map(lambda v,f: v^f, cleanVals,flip))
+    points = list(map(lambda i,d,v: [i,d,v],x,y,dirtyVals))
+    return points
     
         
 
