191cfe475207d1502b47636a5cbb787adfcd922b
Collin Wen <cwen2020@pingry.org>
2018-12-14 15:01:48 -0500

    test.NN updated

--- testNN.py
+++ testNN.py
@@ -9,17 +9,26 @@ from Utils import iterate
 # data: list of data points
 #
 def test(nn, tdata, vdata):
+    training, test = data
+    training = np.array(training)
     vErrorConsec = 0
     cont = True
     tError = []
     vError = []
 
+    #boolean consec
+
     while(cont):
         #train (1) epochs
+        nn.fit(x=training, y=test, batch_size=100, epochs=1, verbose=1)
 
+        # call evaluate - record test & validation error
+        stats = model.evaluate(x=training, y=testing, batch_size=100, epochs=1, verbose=1)
 
-        #record test error
-        #record validation error
+        # record test error
+        tError.append(stats[0])
+        # record validation error
+        vError.append(stats[2])
 
         #terminate if error is under 20% and vErrorConsec is greater than 5
         if(len(vError) > 1 and vError[len(vError)-1] < vError[len(vError)-2]):
