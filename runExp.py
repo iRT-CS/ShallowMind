f7e992bd9c5a4c22b1fac53e73308c371193ad63
Rhea Kapur <rhea.kapur1@gmail.com>
2019-05-15 15:07:08 -0400

    tdata size
    tdata size

--- runExp.py
+++ runExp.py
@@ -21,8 +21,8 @@ import matplotlib.pyplot as plt
 
 coVec = [1,0]
 
-tdata = np.array(gb.getPoints(coVec, 1000, 0, 0, -10, 10, -10, 10))
-vdata = np.array(gb.getPoints(coVec, 1000, 0, 0, -10, 10, -10, 10))
+tdata = np.array(gb.getPoints(coVec, 2000, 0, 0, -10, 10, -10, 10))
+vdata = np.array(gb.getPoints(coVec, 2000, 0, 0, -10, 10, -10, 10))
 
 # plotting the normal dataset, no noise
 # plot the dataset, with noise
