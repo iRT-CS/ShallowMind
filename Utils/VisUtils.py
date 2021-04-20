2b6ee943b9de4b079620af36f2c060771386d9b2
otaylor2023 <75337325+otaylor2023@users.noreply.github.com>
2021-04-19 20:08:14 -0400

    commented vis models

---
+++ Utils/VisUtils.py
@@ -0,0 +1,6 @@
+    def time_convert(self, sec):
+        mins = math.floor(sec // 60)
+        sec = sec % 60
+        hours = math.floor(mins // 60)
+        mins = mins % 60
+        return f"{hours}h, {mins}m, {sec:.2f}s"
