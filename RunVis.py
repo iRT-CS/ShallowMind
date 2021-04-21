160c8ba759822c00211446e2c1e5499912f9b5e6
otaylor2023 <75337325+otaylor2023@users.noreply.github.com>
2021-04-20 23:18:37 -0400

    fixed model naming issue

--- RunVis.py
+++ RunVis.py
@@ -14,11 +14,14 @@ from Datasets import DatasetGenerator as dg
 import Vis as vis
 import time
 
-"""Gets visualizations for a list of models
-:param model_list: list - the list of models to use
-:param dataset_options: ds.DataTypes(.options) - the options for the dataset
-"""
-def runModelBatch(model_list:list, dataset_options:dg.DataTypes):
+
+def visualizeModelBatch(model_list:list, dataset_options:dg.DataTypes):
+    """Gets visualizations for a list of models
+
+        Args:
+            model_list: list - the list of models to use
+            dataset_options: ds.DataTypes(.options) - the options for the dataset
+    """
     plotDataset=True
     for model_id in model_list:
         model_path = MODEL_LOAD_PATH.format(exp_num=exp_num, model_id=model_id)
@@ -29,17 +32,19 @@ def runModelBatch(model_list:list, dataset_options:dg.DataTypes):
             name=model_id, plotDataset=plotDataset)
         plotDataset=False
 
-"""Gets visualizations for checkpoints within a list of models
-Use useAuto and optionally step_percent, OR checkpoint_list
 
-:param model_list: list - the list of models to get checkpoints from
-:param dataset_options: dg.DataTypes(.options) - the options for the dataset
-:param checkpoint_list: list - the checkpoints to use for visualizations
-:param useAuto: boolean - whether to automatically generate the checkpoints or use the checkpoint_list
-:param step_percent: float - if using auto, the percent of the checkpoints to get
-(.1 gets 10% of the checkpoints, in a 100 checkpoint list itll get each tenth one + the first and last)
-"""
-def runCheckpointBatch(model_list:list, dataset_options:dg.DataTypes, checkpoint_list:list=None, useAuto:bool=True, step_percent:float=0.1):
+def visualizeCheckpointBatch(model_list:list, dataset_options:dg.DataTypes, checkpoint_list:list=None, useAuto:bool=True, step_percent:float=0.1):
+    """Gets visualizations for checkpoints within a list of models
+    Use useAuto and optionally step_percent, OR checkpoint_list
+
+    Args:
+        model_list: list - the list of models to get checkpoints from
+        dataset_options: dg.DataTypes(.options) - the options for the dataset
+        checkpoint_list: list - the checkpoints to use for visualizations
+        useAuto: boolean - whether to automatically generate the checkpoints or use the checkpoint_list
+        step_percent: float - if using auto, the percent of the checkpoints to get
+        (.1 gets 10% of the checkpoints, in a 100 checkpoint list itll get each tenth one + the first and last)
+    """
     plotDataset=True
     for model_id in model_list:
         checkpoint_path = CHECKPOINT_LOAD_PATH.format(exp_num=exp_num, model_id=model_id, checkpoint="")
@@ -51,14 +56,18 @@ def runCheckpointBatch(model_list:list, dataset_options:dg.DataTypes, checkpoint
             vis.getVisualizations(model=model, dataset_options=dataset_options, save_path=save_path, name=checkpoint, plotDataset=plotDataset)
             plotDataset=False
 
-"""Gets checkpoints from the checkpoint folder of the given model path
 
-:param model_path: str - the path of the model checkpoint folder
-:param step_percent: float - the percent of checkpoints to get, and the percent between checkpoints
-(.1 gets 10% of the checkpoints, in a 100 checkpoint list itll get each tenth one + the first and last)
-:returns: list - the checkpoint ids as a list
-"""
 def getCheckpoints(model_path:str, step_percent:float=0.2) -> list:
+    """Gets checkpoints from the checkpoint folder of the given model path
+
+    Args:
+        model_path: str - the path of the model checkpoint folder
+        step_percent: float - the percent of checkpoints to get, and the percent between checkpoints
+        (.1 gets 10% of the checkpoints, in a 100 checkpoint list itll get each tenth one + the first and last)
+    
+    Returns:
+        list - the checkpoint ids as a list
+    """
     # sort first by accuracy because epoch isnt zfilled
     all_checkpoints = sorted(os.listdir(model_path), key=lambda dir: dir[-5:])
     checkpoint_list = []
@@ -73,8 +82,8 @@ def getCheckpoints(model_path:str, step_percent:float=0.2) -> list:
 
 def createNetworkSet(layer_shape_list:list, dataset_options:dg.DataTypes, useEarlyStopping:bool=True, saveVisualizations:bool=True):
     """Trains a set of networks for the given layer shape list
+    
     Args:
-
         layer_shape_list: list - the list of shapes for the networks
         useEarlyStopping: bool - whether to use early stopping
         saveVisualizations: bool - whether to save intermediate visualizations
@@ -93,7 +102,7 @@ RAW_SEQUENCE_PATH = ".local\\visualizations\\exp-{exp_num}\\{dataset}\\{model_id
 
 seed = 1
 seeding.setSeed(seed)
-exp_num = 5
+exp_num = 6
 
 
 ds_options = dg.PolynomialOptions()
@@ -121,11 +130,12 @@ dataset = dg.getDataset(dataType=ds_options.name, options=ds_options)
 
 data_save_path = DATAPLOT_SAVE_PATH.format(exp_num=exp_num, dataset=ds_options.name)
 # vis.graphDataset(dataset=dataset, save_path=data_save_path, dataset_options=ds_options, shouldSave=shouldSave)
-runModelBatch(model_list=model_list, dataset_options=ds_options)
+# visualizeModelBatch(model_list=model_list, dataset_options=ds_options)
 # runCheckpointBatch(model_list=model_list, dataset_options=dataset_options, useAuto=True)
 
 layer_shapes_list = [
     [1],
+    [1,1]
     # [6,6,6,6],
     # [1,6,1,6],
     # [6,1,6,1],
@@ -136,10 +146,11 @@ layer_shapes_list = [
     # [1,2,3,4],
     # [3,4,5,6]
 ]
-
+saveVisualizations=True
+useEarlyStopping=True
 # layer_shapes_list = [[1]]
 # start_time = time.time()
-# createNetworkSet(layer_shape_list=layer_shapes_list, dataset_options=ds_options, saveVisualizations=False, useEarlyStopping=False)
+createNetworkSet(layer_shape_list=layer_shapes_list, dataset_options=ds_options, saveVisualizations=saveVisualizations, useEarlyStopping=useEarlyStopping)
 # end_time = time.time()
 # timeStr = VisUtils.time_convert(end_time-start_time)
 # print(f"Model set completed training in {timeStr}")
