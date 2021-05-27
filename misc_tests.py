95318b1fe6ff1909e794a0ca32fba1e49d49d3e7
dmirliss <35306123+dmirliss@users.noreply.github.com>
2021-05-27 14:39:43 -0400

    changed predict_temp name, used for random tests

---
+++ misc_tests.py
@@ -0,0 +1,11 @@
+# import tensorflow as tf
+import Datasets.DatasetGenerator as dg
+import os
+
+
+# model_path = ".local/models/exp-0/model-0002-[4, 4, 4, 4]/model"
+# model = tf.keras.models.load_model(model_path)
+# x, y = dg.getDataset(dg.DataTypes.POLYNOMIAL)
+# model.evaluate(x=x, y=y)
+
+print(os.environ['COMPUTERNAME'])
